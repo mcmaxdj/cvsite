a9596e2d830119fd7aaecc5621ae41edd4c93121
Maksym Sobetskyi <mcmaxdj@gmail.com>
2025-05-19 21:30:36 +0200

    additing skill set list, fixed menu and adding styles edits

--- script.js
+++ script.js
@@ -2,11 +2,22 @@ function toggleMenu(event) {
     event?.stopPropagation();
     document.getElementById('mobileMenu').classList.toggle('show');
 }
+
 function handleBodyClick(event) {
-const nav = document.getElementById('mobileMenu');
-const isMenu = nav.contains(event.target);
-const isBurger = event.target.classList.contains('burger');
+    const nav = document.getElementById('mobileMenu');
+    const isMenu = nav.contains(event.target);
+    const isBurger = event.target.classList.contains('burger');
     if (!isMenu && !isBurger && nav.classList.contains('show')) {
         nav.classList.remove('show');
+    }
+}
+
+function handleNavClick(event) {
+    event.preventDefault();
+    const targetId = event.target.getAttribute('href').substring(1);
+    const targetElement = document.getElementById(targetId);
+    if (targetElement) {
+        targetElement.scrollIntoView({ behavior: 'smooth' });
+        document.getElementById('mobileMenu').classList.remove('show');
     }
 }
